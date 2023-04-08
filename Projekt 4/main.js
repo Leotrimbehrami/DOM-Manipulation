07bbdd38db2e54107ba0efce062ebfe3b60927ef
dci-student <user@user-thinkpad-l15.fritz.box>
2023-04-08 02:00:50 +0200

    finish

--- Projekt 4/main.js
+++ Projekt 4/main.js
@@ -13,6 +13,11 @@ let leadingSeconds = 0
 let leadingMinutes = 0
 let leadingHours = 0
 
+// Variables for set interval & timerstatus
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
 // Stop Watch Function
 
 const stopWatch = () => {
@@ -32,7 +37,7 @@ const stopWatch = () => {
     if(seconds < 10) {
         leadingSeconds = "0" + seconds.toString()
     } else {
-        leadingSeconds =seconds
+        leadingSeconds = seconds
     }
 
     if(minutes < 10) {
@@ -50,4 +55,25 @@ const stopWatch = () => {
     leadingHours + ":" +  leadingMinutes + ":" + leadingSeconds;
 }
 
-window,setInterval(stopWatch, 1000)
+startStopBtn.addEventListener("click", function() {
+    if(timerStatus === "stopped") {
+        timerInterval = window.setInterval(stopWatch, 100);
+        document.getElementById("startStopBtn").innerHTML = `<i class="fa-solid fa-pause" id="pause"></i>`;
+        timerStatus = "started"
+    }else {
+        window.clearInterval(timerInterval);
+        document.getElementById("startStopBtn").innerHTML =`<i class="fa-solid fa-play" id="play"></i>`;
+        timerStatus = "stopped";
+    }
+});
+
+resetBtn.addEventListener("click", function(){
+    
+    window.clearInterval(timerInterval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+
+    document.getElementById("timer").innerHTML = "00:00:00"
+})
+
